8b15f44431f38460db7fa26dc2628d440d93db40
Stan <skukhniy@gmail.com>
2022-09-01 20:00:57 -0400

    error with req.body.password

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,12 @@
 var express = require("express");
 const bcrypt = require("bcrypt");
 var router = express.Router();
+const User = require("../models/user");
 
 const { check, validationResult } = require("express-validator");
 const e = require("express");
+
+const user = require("../models/user");
 
 // get signup page
 router.get("/", function (req, res, next) {
@@ -26,12 +29,26 @@ router.post("/", passwordValidation, async (req, res) => {
 	try {
 		var err = validationResult(req);
 		if (!err.isEmpty()) {
+			console.log("Registration: error w/ password");
 			console.log(err.mapped());
 			res.render("register", { errors: err.mapped().passwordConfirm });
 		} else {
 			console.log("trying bycrpt");
 			// hash password
 			const hashedPassword = await bcrypt.hash(req.body.password, 10);
+			// var user = new User({
+			// 	username: req.body.username,
+			// 	password: hashedPassword,
+			// });
+			// user.save(function (err) {
+			// 	if (err) {
+			// 		return next(err);
+			// 	}
+
+			// 	// successful, then redirect to home page
+			// 	res.redirect("/");
+			// });
+
 			console.log({ username: req.body.username, password: hashedPassword });
 			res.redirect("/");
 		}
